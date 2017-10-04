6cc86fe70cd27e93fab9ac6ba97aa6fc273bb776
ndayoub <nayoub@noc.ac.uk>
2017-10-04 12:18:27 +0200

    updated matlab runtime file names in function.sh

--- src/main/app-resources/node_A/lib/functions.sh
+++ src/main/app-resources/node_A/lib/functions.sh
@@ -8,8 +8,8 @@ ERR_PUBLISH=55
 echo program "started"
 source ${ciop_job_include}
 
-MATLAB_LAUNCHER=$_CIOP_APPLICATION_PATH/node_A/bin/run_ALESrun_jason_CSF.sh
-MATLAB_CMD=$_CIOP_APPLICATION_PATH/node_A/bin/ALESrun_jason_CSF
+MATLAB_LAUNCHER=$_CIOP_APPLICATION_PATH/node_A/bin/run_runALES_jason_CSF.sh
+MATLAB_CMD=$_CIOP_APPLICATION_PATH/node_A/bin/runALES_jason_CSF
 MATLAB_RUNTIME=/data/v90
 #RADS=/home/ndayoub/ALES_RUN/
 RADS=https://store.terradue.com/api/rads/data/
